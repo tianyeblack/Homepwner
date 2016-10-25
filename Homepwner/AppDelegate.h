2c6b2fa2ab75db97104eadb5ad9d9b08b709460d
tianyeblack <tianyeblack@hotmail.com>
2016-10-25 15:05:36 -0700

    Initial Commit

---
+++ Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by Ye Tian on 25/10/2016.
+//  Copyright © 2016 Ye Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
